96b84789e1121d4675ea04c144b07a8135477319
Elliot Pozucek <fall.f777mdk@gmail.com>
2024-04-06 21:07:19 +0200

    Added psutil in install_package.py

--- install_package.sh
+++ install_package.sh
@@ -4,6 +4,6 @@ sudo apt update
 
 sudo apt install -y apache2 python3 python3-pip gnuplot python3-bs4 python3-lxml sqlite3
 
-sudo pip3 install beautifulsoup4
+sudo pip3 install beautifulsoup4 psutil
 
 echo "All packages gave been installed successfully."
